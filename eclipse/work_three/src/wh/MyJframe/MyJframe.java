a368180dadc79ded73fa78af42760677c68cb79d
ShanDaShuMei19 <46954987+ShanDaShuMei19@users.noreply.github.com>
2020-03-14 21:44:43 +0800

    3.14

---
+++ eclipse/work_three/src/wh/MyJframe/MyJframe.java
@@ -0,0 +1,43 @@
+package wh.MyJframe;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MyJframe {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyJframe window = new MyJframe();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MyJframe() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
